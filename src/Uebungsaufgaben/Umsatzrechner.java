b453b78a2238a9bfb6018c47e4b81edfe5a591f5
Bardia D <g.bardia.d@googlemail.com>
2016-09-22 18:23:27 +0200

    Umsatzrechner (geeignet für Quartale und Monate)

--- src/Uebungsaufgaben/Umsatzrechner.java
+++ src/Uebungsaufgaben/Umsatzrechner.java
@@ -1,6 +1,7 @@
 package Uebungsaufgaben;
 
 import static MiniPrograms.GetInput.scanner;
+import java.util.Arrays;
 /**
  *
  * @author firedragon4e
@@ -9,10 +10,11 @@ import static MiniPrograms.GetInput.scanner;
 public class Umsatzrechner{
     public static double[][] setTabelle(){
         int x = Integer.parseInt(scanner("Geben Sie Anzahl der Jahren ein!"))+1;
-        double[][] umsaetze = new double[x][6];
+        int y = Integer.parseInt(scanner("Geben Sie Anzahl der Quartale/Monate ein!"))+2;
+        double[][] umsaetze = new double[x][y];
         for(int i = 0; i < umsaetze.length-1; i++){
             for(int j = 0; j < umsaetze[i].length-2; j++){
-                umsaetze[i][j] = Double.parseDouble(scanner("Geben Sie den Wert f\u00FCr " + j + ". Quartal ein."));
+                umsaetze[i][j] = Double.parseDouble(scanner("Geben Sie den Umsatz f\u00FCr " + (j+1) + ". Quartal/Monat des " + (i+1) + ". Jahres ein."));
             }
         }
         umsaetze = getDurchschnitt(getSumme(getJahresumsatz(umsaetze)));
@@ -25,45 +27,59 @@ public class Umsatzrechner{
             for(int j = 0; j < umsaetze[i].length-2; j++){
                 jahresumsatz += umsaetze[i][j];
             }
-            umsaetze[i][umsaetze.length-1] = jahresumsatz;
+            umsaetze[i][umsaetze[i].length-2] = jahresumsatz;
+        }
+        return umsaetze;
+    }
+    
+    public static double[][] getSumme(double[][] umsaetze){
+        int x = umsaetze[0].length-1, y = umsaetze.length-1;
+        for(int i = 0; i < x; i++){
+            double summe = 0;
+            for(int j = 0; j < y; j++){
+                summe += umsaetze[j][i];
+            }
+            umsaetze[y][i] = summe;
         }
         return umsaetze;
     }
     
     public static double[][] getDurchschnitt(double[][] umsaetze){
         for(int i = 0; i < umsaetze.length; i++){
-            double durchschnitt = 0;
-            for(int j = 0; j < umsaetze[i].length-1; j++){
-                durchschnitt += umsaetze[i][j];
-            }
-            umsaetze[i][umsaetze.length] = durchschnitt;
+            umsaetze[i][umsaetze[i].length-1] = 
+                    umsaetze[i][umsaetze[i].length-2]/(umsaetze[i].length-2)*1.0;
         }
         return umsaetze;
     }
     
-    public static double[][] getSumme(double[][] umsaetze){
-        for(int i = 0; i < umsaetze[0].length-1; i++){
-            double summe = 0;
-            for(int j = 0; j < umsaetze.length-1; j++){
-                summe += umsaetze[j][i];
-            }
-            umsaetze[umsaetze.length][i] = summe;
+    public static void output(double[][] umseatze){
+        String abschnitt;
+        if(umseatze[0].length < 7){
+            abschnitt = "Quartal";
+        }else{
+            abschnitt = "Monat";
         }
-        return umsaetze;
-    }
-    
-    public static void output(double[][] umseatze, int x){
+        String txt = "Jahr\t| ";
+        for(int x = 0; x < umseatze[0].length-2; x++){
+            txt += " " + (x+1) + ". " + abschnitt + "\t| ";
+        }
+        txt += " Jahresumsatz |  Durchschnitt";
+        System.out.println(txt);
         for(int i = 0; i < umseatze.length; i++){
-            for(int j = 0; j < umseatze[i].length-x; j++){
-                System.out.println(umseatze[i][j] + "\t");
+            if(i == umseatze.length-1){
+                System.out.print("Summe\t");
+            }else{
+                System.out.print((i+1) + ". Jahr\t");
             }
-            System.err.println();
+            for(int j = 0; j < umseatze[i].length; j++){
+                System.out.print("|  " + umseatze[i][j] + " \u20AC" + "\t");
+            }
+            System.out.println();
         }
     }
     
     public static void main(String[] args){
         double[][] umseatze = setTabelle();
-        output(umseatze, 0);
-        
+        output(umseatze);
     }
 }
